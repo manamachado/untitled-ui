fa91f9de91a7591ae27e87c4c0b86fe1f959c4c1
Maná Machado <manamachado8@gmail.com>
2024-09-14 13:37:01 -0300

    chore: added hero section component

---
+++ src/app/components/HeroSection/TagFeature.tsx
@@ -0,0 +1,14 @@
+import { FiArrowRight } from 'react-icons/fi';
+
+export function TagFeature() {
+  return (
+    <div className="inline-flex items-center gap-3 p-1 pr-[10px] bg-brand-50 rounded-2xl">
+      <span className="bg-white inline-block h-6 px-[10px] rounded-2xl text-sm/5 font-medium text-brand-700 border border-solid border-brand-200">
+        New feature
+      </span>
+      <a href="#" className="flex items-center gap-1 text-sm/6 font-medium text-brand-700 hover:opacity-50 transition-opacity">
+        Check out the team dashboard <FiArrowRight />
+      </a>
+    </div>
+  );
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,8 +9,16 @@ const config: Config = {
   theme: {
     extend: {
       colors: {
+        'brand-50': '#F9F5FF',
+        'brand-200': '#E9D7FE',
+        'brand-600': '#7F56D9',
+        'brand-700': '#6941C6',
         'gray-100': '#F2F4F7',
-        'gray-600': '#475467'
+        'gray-200': '#E4E7EC',
+        'gray-300': '#D0D5DD',
+        'gray-600': '#475467',
+        'gray-700': '#344054',
+        'gray-900': '#101828'
       },
       maxWidth: {
         'container': '77.5rem'
