0450b5138263c33768f5058e574d4f9b6a5113db
Maná Machado <manamachado8@gmail.com>
2024-09-14 15:31:10 -0300

    chore: added companies

---
+++ src/app/components/Companies/index.tsx
@@ -0,0 +1,20 @@
+import Image from "next/image";
+import { GridContainer } from "../GridContainer";
+
+export function Companies() {
+  return (
+    <section className="py-24">
+      <GridContainer>
+        <p className="text-center text-gray-600 font-medium mb-8">Join 4,000+ companies already growing</p>
+        <div className="flex items-center justify-between">
+          <Image src='/layers.svg' width={146} height={48} alt="Layers logo" />
+          <Image src='/sisyphus.svg' width={169} height={48} alt="Sisyphus logo" />
+          <Image src='/circooles.svg' width={183} height={44} alt="Circooles logo" />
+          <Image src='/catalog.svg' width={160} height={48} alt="Catalog logo" />
+          <Image src='/quotient.svg' width={187} height={44} alt="Quotient logo" />
+        </div>
+        <hr className="mt-24 border-gray-200" />
+      </GridContainer>
+    </section>
+  )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import { Companies } from "./components/Companies";
 import { Header } from "./components/Header";
 import { HeroSection } from "./components/HeroSection";
 
@@ -6,6 +7,7 @@ export default function Home() {
     <>
       <Header />
       <HeroSection />
+      <Companies />
     </>
   );
 }
